5aaaef788605d0d8e6caf4579cf95fcab7fd72cd
Aryeh Harris <harryttd@gmail.com>
2021-10-22 13:14:23 -0400

    comments

--- aws/components/ingress/types.ts
+++ aws/components/ingress/types.ts
@@ -1,14 +1,19 @@
 import * as k8sInputTypes from "@pulumi/kubernetes/types/input"
 
-
-export interface BaseIngressParams {
+/** Describes the base args for resources like ingresses and services that will
+ * be exposed by a load balancer. */
+export interface BaseIngressArgs {
   metadata: k8sInputTypes.meta.v1.ObjectMeta
   /** Prevent pulumi erroring if ingress doesn't resolve immediately */
   pulumiSkipAwait?: boolean
 }
 
-export interface AlbIngressArgs extends BaseIngressParams {
-  /** https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/ingress/annotations/#scheme */
+/** Describes the arguments for an ingress that will be exposed by an
+ * application load balancer. */
+export interface AlbIngressArgs extends BaseIngressArgs {
+  /** Defaults to "internet-facing".
+   * https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/ingress/annotations/#scheme
+   * */
   albScheme?: string
   /** https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/ingress/cert_discovery/#discover-via-ingress-rule-host */
   host?: k8sInputTypes.networking.v1.IngressRule["host"]
@@ -16,6 +21,10 @@ export interface AlbIngressArgs extends BaseIngressParams {
   tls?: k8sInputTypes.networking.v1.IngressTLS[]
 }
 
+/** Describes private internal args used by our ingress components to construct
+ * their custom configs. Example being our ingress component specifying its
+ * ingress backend service. The healthcheck args can be overwritten by a user
+ * specifying `metadata.annotations` */
 export interface InternalAlbIngressArgs {
   ingressServiceBackend: k8sInputTypes.networking.v1.IngressServiceBackend
   /** https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.2/guide/ingress/annotations/#healthcheck-path */
