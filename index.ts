552a44e0b2fadc8ca35488f453d5ed042c2dec3d
Aryeh Harris <harryttd@gmail.com>
2021-11-01 18:12:04 -0400

    Add absolute import workaround to index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,19 @@
-export * from "./aws"
-export * from "./components"
+// Workaround for making absolute import paths work
+// https://github.com/pulumi/pulumi/issues/3061
+import { register, loadConfig } from "tsconfig-paths"
+const config = loadConfig(".")
+if (config.resultType === "failed") {
+  console.log("Could not load tsconfig to map paths, aborting.")
+  process.exit(1)
+}
 
+register({
+  baseUrl: config.absoluteBaseUrl,
+  paths: config.paths,
+})
+
+import * as aws from "./aws"
+
+export { aws }
+export * from "./components"
 export { PulumiSkipAwait } from "./customTypes"
