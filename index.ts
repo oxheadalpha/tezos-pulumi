664654d6deca5ccc39809ba3508518f8f825580f
Aryeh Harris <harryttd@gmail.com>
2021-11-09 14:12:11 -0500

    yarn remove tsconfig-paths

--- index.ts
+++ index.ts
@@ -1,17 +1,3 @@
-// Workaround for making absolute import paths work
-// https://github.com/pulumi/pulumi/issues/3061
-import { register, loadConfig } from "tsconfig-paths"
-const config = loadConfig(".")
-if (config.resultType === "failed") {
-  console.log("Could not load tsconfig to map paths, aborting.")
-  process.exit(1)
-}
-
-register({
-  baseUrl: config.absoluteBaseUrl,
-  paths: config.paths,
-})
-
 import * as aws from "./aws"
 
 export { aws }
