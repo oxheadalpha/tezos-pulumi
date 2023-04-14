3a57f3ae13d66314b0cfacbb917776951133f0be
Aryeh Harris <harryttd@gmail.com>
2023-04-14 13:31:34 -0400

    Remove unnecessary ebs csi IAM policy

--- examples/tutorial/ebsCsi.ts
+++ examples/tutorial/ebsCsi.ts
@@ -32,46 +32,9 @@ const createEbsCsiRole = ({
       })
   )
 
-  new aws.iam.RolePolicyAttachment("ebs-csi-pa-1", {
+  new aws.iam.RolePolicyAttachment("ebs-csi-pa", {
     role: csiRole,
     policyArn: "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
-  })
-
-  const kmsKeyForEncryptionPolicy = new aws.iam.Policy(
-    "ebs-csi-kms-encryption",
-    {
-      policy: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Effect: "Allow",
-            Action: ["kms:CreateGrant", "kms:ListGrants", "kms:RevokeGrant"],
-            Resource: "*",
-            Condition: {
-              Bool: {
-                "kms:GrantIsForAWSResource": "true",
-              },
-            },
-          },
-          {
-            Effect: "Allow",
-            Action: [
-              "kms:Encrypt",
-              "kms:Decrypt",
-              "kms:ReEncrypt*",
-              "kms:GenerateDataKey*",
-              "kms:DescribeKey",
-            ],
-            Resource: "*",
-          },
-        ],
-      },
-    }
-  )
-
-  new aws.iam.RolePolicyAttachment("ebs-csi-pa-2", {
-    role: csiRole,
-    policyArn: kmsKeyForEncryptionPolicy.arn,
   })
 
   return csiRole
